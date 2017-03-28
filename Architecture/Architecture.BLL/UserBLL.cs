0d916580acfb386737a97c7f77729d0b075e07df
thinh <thanhthinhcntt@gmail.com>
2017-03-28 20:59:48 +0700

    update project

---
+++ Architecture/Architecture.BLL/UserBLL.cs
@@ -0,0 +1,48 @@
+﻿using Architecture.DAL;
+using Architecture.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.BLL
+{
+    public class UserBLL
+    {
+        private UserDAL _userDAL;
+
+        public UserBLL()
+        {
+            _userDAL = new UserDAL();
+        }
+        public User GetUserByID(int id)
+        {
+            User _user;
+            DataTable dt = _userDAL.GetUserById(id);
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                int userID = int.Parse(dt.Rows[0]["id"].ToString());
+                string userName = dt.Rows[0]["username"].ToString();
+                string password_hash = dt.Rows[0]["password_hash"].ToString();
+                DateTime createAt = DateTime.Parse(dt.Rows[0]["created_at"].ToString());
+                DateTime lastLogin = DateTime.Parse(dt.Rows[0]["last_login"].ToString());
+                DateTime lastUpdate = DateTime.Parse(dt.Rows[0]["last_updated"].ToString());
+                int status = int.Parse(dt.Rows[0]["status"].ToString());
+                int userType = int.Parse(dt.Rows[0]["usertype"].ToString());
+                string email = dt.Rows[0]["email"].ToString();
+                _user = new User(userID, userName, "", password_hash, createAt, lastLogin, status, lastUpdate, email, userType);
+
+                return _user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
+
+
---
+++ Architecture/Architecture.DAL/UserDAL.cs
@@ -0,0 +1,30 @@
+﻿using Architecture.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.DAL
+{
+  public  class UserDAL
+    {
+        private DbConnection _dbconnection=null;
+        public UserDAL()
+        {
+            _dbconnection = new DbConnection();
+        }
+        public DataTable GetUserById(int id)
+        {
+            string sql = @"";
+            DataTable dt = _dbconnection.ExecuteQuery(sql, new ParameterPair[]
+            {
+                new ParameterPair("@id",id.ToString())
+            });
+            return dt;
+
+        }
+
+    }
+}
